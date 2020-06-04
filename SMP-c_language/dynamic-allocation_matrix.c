67b3f88737ae43e46bcdaf2cc0cacdbf287f1545
Seenbeom Lee <mail@seenblee.com>
2020-06-04 23:30:49 +0900

    1.40: practice: 2020.05.28 / 8th
    #make 2d matrix with dynamic-allocation

--- SMP-c_language/dynamic-allocation_matrix.c
+++ SMP-c_language/dynamic-allocation_matrix.c
@@ -1,2 +1,23 @@
 #include<stdio.h>
 #include<stdlib.h>
+
+int main() {
+	int size;
+	int** matrix;
+	
+	printf("Enter the size of square: ");
+	scanf("%d", &size);
+
+	matrix = (int**)malloc(size * sizeof(int*));
+	for (int i = 0; i < size; i++)
+		matrix[i] = (int*)calloc(size, sizeof(int));
+
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			if (i == j) printf("  0");
+			else if (i > j) printf(" -1");
+			else printf("  1");
+		}
+		printf("\n");
+	}
+}
