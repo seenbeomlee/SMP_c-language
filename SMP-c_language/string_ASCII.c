a548f5cf43d953a02eb9fd6070a4f813070dc898
Seenbeom Lee <mail@seenblee.com>
2020-06-04 23:50:38 +0900

    1.52: practice: 2020.06.04 / 8th
    #string to UPPER, string to lower

---
+++ SMP-c_language/string_ASCII.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+char* str_to_upper(char* str) {
+	char* tmp = str;
+
+	while (*tmp != NULL) {
+		if (*tmp > 96 && *tmp < 123)
+			*tmp -= 32;
+		tmp++;
+	}
+	return str;
+}
+
+char* str_to_lower(char* str) {
+	char* tmp = str;
+
+	while (*tmp != NULL) {
+		if (*tmp > 64 && *tmp < 91)
+			*tmp += 32;
+		tmp++;
+	}
+	return str;
+}
+
+int main() {
+	char str[100];
+
+	printf("Input string: ");
+	gets(str);
+
+	printf("Upper: %s\n", str_to_upper(str));
+	printf("Lower: %s\n", str_to_lower(str));
+
+	return 0;
+}
